c0cdbf3651e5efc79bdea7efa267a7d6623e3261
manish45-comp <81609620+manish45-comp@users.noreply.github.com>
2023-10-25 16:30:23 +0530

    2.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import peoples from "./Data";
 
-function App() {
+const App = () => {
+  const [people, setPeoples] = useState(peoples);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-com">
+      <div className="sticky">
+        <div className="container">
+          <div className="title">
+            <h1>{people.length} Birthday Today</h1>
+          </div>
+          <div className="card-wrapper">
+            {people.length > 0 ? (
+              " "
+            ) : (
+              <h1 className="cleared">No Birthdays</h1>
+            )}
+            {people.map((p) => {
+              return (
+                <div className="person" key={p.id}>
+                  <img src={p.image} alt="profile"></img>
+                  <div>
+                    <h4>{p.fullName}</h4>
+                    <h4 className="date">
+                      <i className="fa-solid fa-cake-candles"></i>
+                      {p.Dob}
+                    </h4>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <button
+            className="btn"
+            onClick={() => {
+              let btn = document.querySelector(".btn");
+              console.log(btn);
+              btn.classList.add("clear");
+              setPeoples([]);
+            }}
+          >
+            <i className="fa-solid fa-trash"></i>
+            <span>Clear</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
